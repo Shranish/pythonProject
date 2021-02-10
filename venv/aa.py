333a5e380835b2bd9015482b16fc9f79526b1895
asus <shranish123@gmail.com>
2021-02-10 12:21:09 +0545

    addition

---
+++ venv/aa.py
@@ -0,0 +1,10 @@
+m1=int(input('Enter your marks of 1st subject'))
+m2=int(input('Enter your marks of 2nd subject'))
+m3=int(input('Enter your marks of 3rd subject'))
+m4=int(input('Enter your marks of 4th subject'))
+m5=int(input('Enter your marks of 5th subject'))
+
+avg_marks=(m1+m2+m3+m4+m5)/5
+per=((m1+m2+m3+m4+m5)/500)*100
+print(avg_marks)
+print(per)
---
+++ venv/ss.py
@@ -0,0 +1,5 @@
+name='Johson Paul'
+print(name[:])
+print(name[1:])
+print(name[-5:0:-1])
+print(name[-1:-2])
