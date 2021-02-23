e2a206a4aec28ec87e4a1df6d28fccf7c37de214
asus <shranish123@gmail.com>
2021-02-23 10:17:24 +0545

    houranmin

---
+++ Pythonlab/hours_and_min.py
@@ -0,0 +1,4 @@
+N=int(input('Enter the number of min that is passed since midnight:'))
+hour=N//60
+min=N%60
+print('The time is ',hour,':',min)
---
+++ git.py
@@ -0,0 +1,3 @@
+print("My name is Shranish Shrestha. And my age is 19")
+print(f"My name is Shranish Shrestha. And my age is 19")
+print("My name is Shranish Shrestha." + " And my age is 19")
