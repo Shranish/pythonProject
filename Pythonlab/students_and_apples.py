8252e34b95f93c031553249d786aa7df3cb5fe91
asus <shranish123@gmail.com>
2021-02-23 09:53:07 +0545

    studentsandapple

---
+++ Pythonlab/students_and_apples.py
@@ -0,0 +1,6 @@
+N=int(input('Enter the number of students :'))
+K=int(input('Enter the number of apples :'))
+n=K//N
+print('The number of apples that each student gets =',n)
+r=K%N
+print('The number of remaining apples in the basket =',r)
