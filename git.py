a52470a564358fc16e0b401777ef472b222db1bb
asus <shranish123@gmail.com>
2021-03-02 09:33:20 +0545

    terminal check

--- git.py
+++ git.py
@@ -1,3 +1,21 @@
-print("My name is Shranish Shrestha. And my age is 19")
-print(f"My name is Shranish Shrestha. And my age is 19")
-print("My name is Shranish Shrestha." + " And my age is 19")
+# i=10
+# while i>=1:
+#     print(i)
+#     i=i-1
+# print('Program is over')
+
+# i=1
+# while i<=50:
+#     if i%2==0:
+#         print(i,'It is even')
+#     else:
+#         print(i,'It is odd')
+#     i=i+1
+
+# wap to find the sum of elements of list using while loop
+sum=0
+lst=[10,31,37,40,56,63,70]
+for i in lst:
+    sum=sum+i
+print('the sum is ',sum)
+
