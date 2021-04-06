6cd9b1c69a55b99195efd15ebda1bdef0b9ab7eb
asus <shranish123@gmail.com>
2021-04-06 09:39:33 +0545

    qno4

---
+++ Pythonlab2/Qno.4.py
@@ -0,0 +1,7 @@
+temperature=int(input('Enter the tempersture :'))
+if temperature>30:
+    print('It is a hot day.')
+elif temperature<10:
+    print('It is a cold day.')
+else:
+    print('It is neither hot nor cold.')
