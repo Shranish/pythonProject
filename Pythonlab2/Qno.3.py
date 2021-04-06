0467b67beca1d3043f521381b6c161025d12ad61
asus <shranish123@gmail.com>
2021-04-06 09:35:09 +0545

    qno3

---
+++ Pythonlab2/Qno.3.py
@@ -0,0 +1,8 @@
+price=1000000
+credit= False
+if credit:
+    payment1=0.1*price
+    print(f'The amount to be paid is {payment1}')
+else:
+    payment2=0.2*price
+    print(f'The amount to be paid is {payment2}')
